512f567166cfd1a609fe2dd4c9b98e4ad32a0cb0
LiiaZubova07 <vardlitmo@gmail.com>
2022-06-23 20:14:03 +0300

    add: first step for application

---
+++ js/main.js
@@ -0,0 +1,21 @@
+'use strict';
+//тут будет числовое знчение из-за +; а из-за prompt будет задаваться вопрос
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
