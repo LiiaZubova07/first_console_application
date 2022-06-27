1aab82fc0020c3c4441c71fc1f27ea4cabb7b686
LiiaZubova07 <vardlitmo@gmail.com>
2022-06-27 19:26:05 +0300

    add js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 //тут будет числовое знчение из-за +; а из-за prompt будет задаваться вопрос
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -10,12 +10,36 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-  b = prompt("На сколько оцените его?", ""),
-  c = prompt("Один из последних просмотренных фильмов?", ""),
-  d = prompt("На сколько оцените его?", "");
+/*const a = prompt('Один из последних просмотренных фильмов?', ''),
+  b = prompt('На сколько оцените его?', ''),
+  c = prompt('Один из последних просмотренных фильмов?', ''),
+  d = prompt('На сколько оцените его?', '');
 
 personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+personalMovieDB.movies[c] = d;*/
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
+
+  //проверка на null. Не null, не пустая строка, кол-во символов не меньше 50
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log('Просмотрено мало фильмов');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+  console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
